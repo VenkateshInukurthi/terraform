c3571b09576adee19cc659109aa4557de799586c
Lav-Venkat <inukurthivenkatesh@gmail.com>
2025-02-11 21:33:01 +0530

    Updated data-sources

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "joindevops" {
     owners = ["973714476881"]
     filter {
       name = "name"
-      values = [RHEL-9-DevOps-Practice]
+      values = ["RHEL-9-DevOps-Practice"]
     }
 
     filter {
