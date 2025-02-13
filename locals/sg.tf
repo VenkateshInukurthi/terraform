5b2595ae77260550bc10566e5668140afd67be59
Lav-Venkat <inukurthivenkatesh@gmail.com>
2025-02-13 23:26:16 +0530

    adding security group code for locals

---
+++ locals/sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "locals_sg" {
+    name = "locals_sg"
+    description = "Allow port 22"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    
+  
+}
