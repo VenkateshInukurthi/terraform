4fd1d1ca5d17f52122cebfaac72af5a7b9a4d5ae
Lav-Venkat <inukurthivenkatesh@gmail.com>
2025-02-13 23:23:01 +0530

    adding ec2 instance creation for locals

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -1,4 +1,10 @@
 resource "aws_instance" "locals_instance" {
-    
+    count = length(local.instances)
+    ami = data.aws_ami.locals.id
+    vpc_security_group_ids = []
+    instance_type = "t3.micro"
+    tags = {
+      Name = local.instances[count.index]
+    }
   
 }
