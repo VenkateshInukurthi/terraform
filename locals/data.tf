ac31ef45da33a7ae5c97a04c655978353571f381
Lav-Venkat <inukurthivenkatesh@gmail.com>
2025-02-13 23:20:42 +0530

    adding locals code

---
+++ locals/data.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "locals" {
+
+    most_recent = true
+    owners = ["973714476881"]
+
+    filter {
+      name = "name"
+      values = "RHEL-9-DevOps-Practice"
+    }
+
+    filter {
+      name = "root-device-type"
+      values = ["ebs"]
+    }
+
+    filter {
+      name = "virtualization-device"
+      values = ["hvm"]
+    }
+  
+}
---
+++ locals/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  domain_name = "masterdevops.cloud"
+  project = "expense"
+  environment = "dev"
+  zone_id = "Z03980142KYPX1BJQWBBZ"
+  instances = ["mysql", "backend", "frontend"]
+}
