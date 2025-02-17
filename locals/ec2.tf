d03969be8fe34084538911f43fa7c28067ae9a5e
Lav-Venkat <inukurthivenkatesh@gmail.com>
2025-02-17 22:47:22 +0530

    updated local instance type

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "locals_instance" {
     count = length(var.instances)
     ami = data.aws_ami.locals.id
     vpc_security_group_ids = []
-    instance_type = "t3.micro"
+    instance_type = local.instance_type #"t3.micro"
     tags = {
       Name = var.instances[count.index]
     }
--- locals/locals.tf
+++ locals/locals.tf
@@ -3,5 +3,5 @@ locals {
   project = "expense"
   environment = "dev"
   zone_id = "Z03980142KYPX1BJQWBBZ"
-  
+  instance_type = local.environment == "prod" ? "t3.micro" : "t3.small"
 }
